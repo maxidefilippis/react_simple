1d78d997ff89b3f5beabaa8c47d75b8671a801d5
maxidefilippis <maxidefilippis@gmail.com>
2021-12-19 21:06:25 +0100

    feat: actions constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,9 @@
+// Pagination constants
 export const BACK_LIMIT = 1;
 export const NEXT_LIMIT = 6;
 export const FETCH_LIMIT = 10;
+
+// Actions constants
+export const ADD_FAVORITE = "ADD_FAVORITE"
+export const REMOVE_FAVORITE = "REMOVE_FAVORITE"
+export const CLEAN_FAVORITES = "CLEAN_FAVORITES"
