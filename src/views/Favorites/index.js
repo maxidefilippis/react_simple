403cbece67a65e4980c348dc48dec5b7ee82ba18
maxidefilippis <maxidefilippis@gmail.com>
2021-12-19 17:34:17 +0100

    feat: create basic views

---
+++ src/views/Favorites/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../layout";
+
+const Favorites = () => {
+  return (
+    <Layout>
+      <div>Favorites</div>
+    </Layout>
+  );
+};
+
+export default Favorites;
---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Favorites from "./Favorites";
+import NotFound from "./NotFound";
+
+export { Home, Favorites, NotFound };
