7e3f2c666a314c5d76f6d2aeae6a0a732b2b296c
maxidefilippis <maxidefilippis@gmail.com>
2021-12-19 21:06:53 +0100

    feat: add reducer

---
+++ src/state/favoritesReducer/reducer.js
@@ -0,0 +1,27 @@
+import {
+  ADD_FAVORITE,
+  REMOVE_FAVORITE,
+  CLEAN_FAVORITES,
+} from "../../constants";
+
+const initialState = {
+  favorites: [],
+};
+
+const favoritesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_FAVORITE:
+      return { ...state, favorites: [...state.favorites, action.payload] };
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        favorites: state.favorites.filter((element) => element !== action.payload),
+      };
+    case CLEAN_FAVORITES:
+      return { ...state, favorites: [] };
+    default:
+      return state;
+  }
+};
+
+export default favoritesReducer;
