d031880b916769c180dd97f7df77d5ba5eddf1bf
maxidefilippis <maxidefilippis@gmail.com>
2021-12-19 23:49:54 +0100

    feat: favorite view

--- src/views/Favorites/index.js
+++ src/views/Favorites/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Layout from "../../layout";
+import { PlanetsMapping } from "../../components";
 
 const Favorites = () => {
+  const { favorites } = useSelector(({ favoritesReducer }) => favoritesReducer);
   return (
     <Layout>
-      <div>Favorites</div>
+      <div>Favorites: {favorites.length}</div>
+      <PlanetsMapping planets={favorites} />
     </Layout>
   );
 };
