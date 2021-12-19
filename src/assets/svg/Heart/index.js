c4fd81fc62b32885b1c32531c0eb8bfd900a0cba
maxidefilippis <maxidefilippis@gmail.com>
2021-12-19 22:15:08 +0100

    feat: add svg folder

---
+++ src/assets/svg/Heart/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Heart = ({ onClick, heartFill }) => {
+  const notFilled = heartFill === "none";
+  return (
+    <svg
+      onClick={onClick}
+      xmlns="http://www.w3.org/2000/svg"
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke={notFilled ? "#2c3e50" : "red"}
+      fill={heartFill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+    </svg>
+  );
+};
+
+export default Heart;
