af9cc45c10c6c2a4bcc1cc9f833e0086279da104
maxidefilippis <maxidefilippis@gmail.com>
2021-12-19 21:08:00 +0100

    feat: suscribe home component to store

--- src/views/Home/HomeComponents.js
+++ src/views/Home/HomeComponents.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BACK_LIMIT, NEXT_LIMIT } from "../../constants";
 import { Button } from "../../components";
+import {
+  addFavorite,
+  removeFavorite,
+} from "../../state/favoritesReducer/actions";
 
 export const PaginationControls = ({ controls, pagination }) => (
   <div>
@@ -28,23 +33,36 @@ export const PlanetsMapping = ({ planets }) => (
   </div>
 );
 
-export const PlanetCard = ({ planet }) => (
-  <div>
-    <h3>{planet.name}</h3>
-    <div>
-      <h4>Diametro:</h4> <span>{planet.diameter}</span>
+export const PlanetCard = ({ planet }) => {
+  const { favorites } = useSelector(({ favoritesReducer }) => favoritesReducer);
+  const dispatch = useDispatch();
+  console.log({ favorites });
+
+  const handleFavorite = () => {
+    if (favorites.includes(planet.name)) {
+      dispatch(removeFavorite(planet.name));
+    } else {
+      dispatch(addFavorite(planet.name));
+    }
+  };
+  return (
+    <div onClick={handleFavorite}>
+      <h3>{planet.name}</h3>
+      <div>
+        <h4>Diametro:</h4> <span>{planet.diameter}</span>
+      </div>
+      <div>
+        <h4>Climas:</h4>
+        {planet.climates.map((clima, index) => (
+          <span key={index}>{clima} </span>
+        ))}
+      </div>
+      <div>
+        <h4>Terrenos:</h4>
+        {planet.terrains.map((terrain, index) => (
+          <span key={index}>{terrain} </span>
+        ))}
+      </div>
     </div>
-    <div>
-      <h4>Climas:</h4>
-      {planet.climates.map((clima, index) => (
-        <span key={index}>{clima} </span>
-      ))}
-    </div>
-    <div>
-      <h4>Terrenos:</h4>
-      {planet.terrains.map((terrain, index) => (
-        <span key={index}>{terrain} </span>
-      ))}
-    </div>
-  </div>
-);
+  );
+};
