cd1009d66952f2892bcb406065325cf5ca949673
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 07:13:48 +0100

    fix: improve heart behavior

--- src/assets/svg/Heart/index.js
+++ src/assets/svg/Heart/index.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "./index.scss";
 
 const Heart = ({ onClick, heartFill }) => {
-  const notFilled = heartFill === "none";
   return (
     <svg
+      className={`heart__${heartFill}`}
       onClick={onClick}
       xmlns="http://www.w3.org/2000/svg"
       width="44"
       height="44"
       viewBox="0 0 24 24"
       strokeWidth="1.5"
-      stroke={notFilled ? "#2c3e50" : "red"}
-      fill={heartFill}
+      stroke="none"
+      fill="none"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
