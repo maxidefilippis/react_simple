f606230fcee58f5d0a48173a93189f21d38e0bfb
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 07:40:05 +0100

    chore: improve error component

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { Title } from "..";
+import Layout from "../../layout";
 
-const Error = ({ message }) => <p>Error...{message}</p>;
+const Error = ({ message }) => (
+  <Layout>
+    <Title name="HOME" />
+    <p>Error...{message}</p>
+  </Layout>
+);
 
 export default Error;
