bc5d4f86996ec1400a7d10309db138dd99f4034b
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 07:45:43 +0100

    feat: logic for favorites

--- src/views/Favorites/index.js
+++ src/views/Favorites/index.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import "./index.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { Button, PlanetsMapping, Title } from "../../components";
+import { cleanFavorites } from "../../state/favoritesReducer/actions";
 import Layout from "../../layout";
-import { PlanetsMapping } from "../../components";
 
 const Favorites = () => {
   const { favorites } = useSelector(({ favoritesReducer }) => favoritesReducer);
+  const dispatch = useDispatch();
+  const areThereFavorites = favorites.length > 0;
+
+  const FavoritesFooter = () =>
+    areThereFavorites ? (
+      <Button
+        className="button favorites__button"
+        name="Clean Favorites"
+        onClick={() => dispatch(cleanFavorites())}
+      />
+    ) : (
+      <p className="favorites__message">NO FAVORITES YET</p>
+    );
+
   return (
     <Layout>
-      <div>Favorites: {favorites.length}</div>
-      <PlanetsMapping planets={favorites} />
+      <Title name="FAVORITES" />
+      {areThereFavorites ? <PlanetsMapping data={favorites} /> : null}
+      <FavoritesFooter />
     </Layout>
   );
 };
