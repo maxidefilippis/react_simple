6e2d8a7c801346a0337db2b33fc98143b563c55c
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 07:42:39 +0100

    feat: add react toastify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
 import { ApolloProvider } from "@apollo/client";
 import Routes from "./routes";
 import client from "./api";
 import store from "./state";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.scss";
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
     <ApolloProvider client={client}>
       <Provider store={store}>
         <BrowserRouter>
+          <ToastContainer />
           <Routes />
         </BrowserRouter>
       </Provider>
