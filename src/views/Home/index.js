5787177866eeefed31d1819783c3166db28143c4
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 07:46:15 +0100

    fix: improve home for searchs

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_PLANETS } from "../../graphql/queries";
 import { FETCH_LIMIT } from "../../constants";
-import { PaginationControls, PlanetsMapping } from "../../components";
+import { Button, PaginationControls, PlanetsMapping } from "../../components";
 import { Title, Error, Loading } from "../../components";
 import Layout from "../../layout";
+import { useSelector, useDispatch } from "react-redux";
+import { clearSearch } from "../../state/planetsReducer/actions";
 
 const Home = () => {
+  const dispatch = useDispatch();
   const [pagination, setPagination] = useState(1);
+  const [searchNotNull, setSearchNotNull] = useState(false);
   const { data, error, loading } = useQuery(GET_PLANETS, {
     variables: { first: pagination * FETCH_LIMIT, last: FETCH_LIMIT },
   });
+  const searchedPlanets = useSelector((state) => state.planetsReducer.planets);
+  const { search } = useSelector((state) => state.planetsReducer);
+
+  useEffect(() => {
+    if (searchedPlanets.length > 0 && search) {
+      setSearchNotNull(true);
+    } else {
+      setSearchNotNull(false);
+    }
+  }, [searchedPlanets, search]);
 
   if (loading) return <Loading />;
   if (error) return <Error message={error?.message} />;
@@ -21,12 +35,19 @@ const Home = () => {
     ["next"]: () => setPagination(pagination + 1),
   };
 
+  const resetSearch = () => {
+    dispatch(clearSearch());
+  };
+
   return (
     <Layout>
-      <Title name="Home" />
-      <PaginationControls controls={controls} pagination={pagination} />
-      <PlanetsMapping planets={planets} />
-      <PaginationControls controls={controls} pagination={pagination} />
+      <Title name={searchNotNull ? "SEARCH" : "HOME"} />
+      <PlanetsMapping search={searchedPlanets} data={planets} />
+      {searchNotNull ? (
+        <Button name="CLEAR SEARCH" onClick={resetSearch} />
+      ) : (
+        <PaginationControls controls={controls} pagination={pagination} />
+      )}
     </Layout>
   );
 };
