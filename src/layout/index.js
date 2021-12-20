2158f98b19a5a8f7699abd870a03a942fdc98c41
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 07:44:53 +0100

    chore: layout improve & styles

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,18 +1,33 @@
 import React from "react";
+import "./index.scss";
 import { Link } from "react-router-dom";
+import { Search } from "../components";
+import { useSelector } from "react-redux";
+
+const Links = () => {
+  const { favorites } = useSelector(({ favoritesReducer }) => favoritesReducer);
+  return (
+    <nav className="layout__links">
+      <Link to="/">HOME</Link>
+      <Link to="/favorites">FAVORITES: {favorites.length}</Link>
+    </nav>
+  );
+};
+
+const Footer = () => (
+  <footer className="layout__footer">Ⓒ Developed by Maxi De Filippis.</footer>
+);
 
 const Layout = ({ children }) => (
-  <div>
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/favorites">Favorites</Link>
-      </nav>
-    </header>
-    <main>{children}</main>
-    <footer>
-      Proyecto desarrollado por Maxi De Filippis.
-    </footer>
+  <div className="layout">
+    <main className="layout__main">
+      <header className="layout__header">
+        <Links />
+        <Search />
+      </header>
+      <section className="layout__content">{children}</section>
+    </main>
+    <Footer />
   </div>
 );
 
