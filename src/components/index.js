a9a383e62c4a6e8be1a7ef238d6b8bde591619b0
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 07:39:13 +0100

    feat: input component

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,21 @@
 import Button from "./Button";
 import Error from "./Error";
+import Input from "./Input";
 import Loading from "./Loading";
 import PaginationControls from "./PaginationControls";
 import PlanetCard from "./PlanetsCards/PlanetCard";
 import PlanetsMapping from "./PlanetsCards/PlanetsMapping";
+import Search from "./Search";
 import Title from "./Title";
 
 export {
   Button,
   Error,
+  Input,
   Loading,
   PaginationControls,
   PlanetCard,
   PlanetsMapping,
+  Search,
   Title,
 };
