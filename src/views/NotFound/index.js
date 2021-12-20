92e7a77fd8b4ed5f49de6002ddd5840cb05ed686
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 08:27:20 +0100

    fix: style details

--- src/views/NotFound/index.js
+++ src/views/NotFound/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Title } from "../../components";
 import Layout from "../../layout";
 
 const NotFound = () => {
   return (
     <Layout>
-      <div>NotFound</div>
+      <Title name="ERROR #404" />
+      <p className="favorites__message">NOT FOUND</p>
     </Layout>
   );
 };
