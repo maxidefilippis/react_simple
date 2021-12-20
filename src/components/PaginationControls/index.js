82b7d8035a80ad47e226bd1d8d633c7e6ce50e55
maxidefilippis <maxidefilippis@gmail.com>
2021-12-20 07:40:41 +0100

    chore: pagination controls styles

--- src/components/PaginationControls/index.js
+++ src/components/PaginationControls/index.js
@@ -1,22 +1,25 @@
 import React from "react";
+import "./index.scss";
 import { BACK_LIMIT, NEXT_LIMIT } from "../../constants";
 import { Button } from "../../components";
 
 const PaginationControls = ({ controls, pagination }) => (
-  <div>
-    <Button
-      name="Back"
-      onClick={controls["back"]}
-      disabled={pagination === BACK_LIMIT}
-    />
+  <div className="paginationControls">
+    <div className="paginationControls__buttons">
+      <Button
+        name="Back"
+        onClick={controls["back"]}
+        disabled={pagination === BACK_LIMIT}
+      />
+      <Button
+        name="Next"
+        onClick={controls["next"]}
+        disabled={pagination === NEXT_LIMIT}
+      />
+    </div>
     <p>
       Page: {pagination} of {NEXT_LIMIT}
     </p>
-    <Button
-      name="Next"
-      onClick={controls["next"]}
-      disabled={pagination === NEXT_LIMIT}
-    />
   </div>
 );
 
