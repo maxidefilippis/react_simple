4534008d39e9aab5b4501c6c073550b0dfe02e1a
maxidefilippis <maxidefilippis@gmail.com>
2021-12-22 21:47:16 +0100

    fix: clean errors

--- src/assets/svg/Favorite/index.js
+++ src/assets/svg/Favorite/index.js
@@ -3,15 +3,15 @@ import React from "react";
 const Favorite = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon icon-tabler icon-tabler-star"
+    className="icon icon-tabler icon-tabler-star"
     width="22"
     height="22"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="#2c3e50"
     fill="none"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
